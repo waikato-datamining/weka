aabef99c57b741402876d49d29c6bd0d5950e267
Peter Reutemann <fracpete@gmail.com>
2018-10-23 14:44:22 +1300

    mirror URL now uses https

--- src/main/java/weka/core/WekaPackageManager.java
+++ src/main/java/weka/core/WekaPackageManager.java
@@ -483,10 +483,9 @@ public class WekaPackageManager {
       return;
     }
 
+    String mirrorListURL =
+      "https://www.cs.waikato.ac.nz/ml/weka/packageMetaDataMirror.txt";
     try {
-      String mirrorListURL =
-        "http://www.cs.waikato.ac.nz/ml/weka/packageMetaDataMirror.txt";
-
       URLConnection conn = null;
       URL connURL = new URL(mirrorListURL);
 
@@ -519,6 +518,8 @@ public class WekaPackageManager {
         }
       }
     } catch (Exception ex) {
+      System.err.println("Failed to access package mirror: " + mirrorListURL);
+      ex.printStackTrace();
       log(weka.core.logging.Logger.Level.WARNING,
         "[WekaPackageManager] The repository meta data mirror file seems "
           + "to be unavailable (" + ex.getMessage() + ")");
